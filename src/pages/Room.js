37f889e94a979e9148440df78cf21440d0d72a27
Carlos Lancha <carlos.lancha@liferay.com>
2017-08-02 04:57:28 +0200

    Create Room Page

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,12 +1,87 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+
+import Counter from '../components/Counter.js';
+import NeonGrid from '../components/NeonGrid.js';
+import Terminal from '../components/Terminal.js';
+import socket from '../components/socket.js';
+
+import './Room.css';
 
 class Room extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			clue: '',
+			countingClues: []
+		};
+	}
+
+	componentDidMount() {
+		socket.on('countersChanged', (event) => {
+			let mainCounter = event.counters.shift();
+			let extraCounter = event.counters.shift();
+
+			this.setState(
+				{
+					extraCounter: extraCounter,
+					mainCounter: mainCounter
+				}
+			);
+		});
+
+		socket.on('newClue', (event) => {
+			this.setState(
+				{
+					clue: event.clue,
+					countingClues: event.countingClues
+				}
+			);
+		});
+
+		socket.emit('getClue');
+		socket.emit('getCounters');
+	}
+
+	componentWillUnmount() {
+		socket.removeListener('countersChanged');
+		socket.removeListener('newClue');
+	}
+
 	render() {
+		let countersClasses = 'counters' + (this.state.extraCounter ? ' marginless' : '');
+
 		return (
-			<div className="Room">
-				Room
-				<Link to="/admin">Go To Admin</Link>
+			<div className="room">
+				<div className="clueCounters">
+					{this.state.countingClues.map((clue, i) => {
+						return <div className="clueCounter">
+							<span className="glyphicon glyphicon-star" aria-hidden="true"></span>
+						</div>
+					})}
+				</div>
+				<div className={countersClasses}>
+					{this.state.mainCounter &&
+						<Counter className="text-center" time={this.state.mainCounter.time}	/>
+					}
+
+					{this.state.extraCounter &&
+						<Counter className="extra-counter text-center" time={this.state.extraCounter.time} trim="true">
+							<p>BONUS TIME!</p>
+						</Counter>
+					}
+				</div>
+
+				{this.state.clue &&
+					<div className="container clue">
+						<Terminal>
+							{this.state.clue}
+						</Terminal>
+					</div>
+				}
+
+				<NeonGrid />
 			</div>
 		);
 	}
